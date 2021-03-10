e4c59ca9675ed202162a099b1d889f5ed4c1314c
廖昱晴 <pulin.liao@ctbcsec.com>
2021-03-10 17:25:42 +0800

    SwiftUI Transition practice

--- SwiftUITransition/ContentView.swift
+++ SwiftUITransition/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var show = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 300, height: 300)
+                .foregroundColor(.green)
+                .overlay(
+                Text("Show details")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .bold()
+                    .foregroundColor(.white)
+                )
+        }
+        .onTapGesture {
+            withAnimation(Animation.spring()) {
+                self.show.toggle()
+            }
+        }
+        
+        if show {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 300, height: 300)
+                .foregroundColor(.purple)
+                .overlay(
+                Text("Well, here is the details")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .bold()
+                    .foregroundColor(.white)
+                )
+                .transition(.asymmetric(insertion: .scale(scale: 0, anchor: .bottom), removal: .offset(x: -600, y: 0)))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension AnyTransition {
+    static var offsetScaleOpacity: AnyTransition {
+        AnyTransition.offset(x: -600, y: 0).combined(with: .scale).combined(with: .opacity)
     }
 }
