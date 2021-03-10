43e3f8e547057f617a547d97294b9a1d1f27329e
廖昱晴 <pulin.liao@ctbcsec.com>
2021-03-10 16:57:53 +0800

    Initial Commit

---
+++ SwiftUITransition/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITransition
+//
+//  Created by 廖昱晴 on 2021/3/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITransition/SwiftUITransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITransitionApp.swift
+//  SwiftUITransition
+//
+//  Created by 廖昱晴 on 2021/3/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
